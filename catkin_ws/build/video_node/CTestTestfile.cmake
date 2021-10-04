af6ee7d48a4409ec469b8fe9b34837bf9f4a223d
Alessia Borghini <80713403+ABorghini@users.noreply.github.com>
2021-10-04 12:23:25 +0200

    Add files via upload

---
+++ catkin_ws/build/video_node/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/alessia/Desktop/Loop Closure/loop-closure-labiagi/catkin_ws/src/video_node
+# Build directory: /home/alessia/Desktop/Loop Closure/loop-closure-labiagi/catkin_ws/build/video_node
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
